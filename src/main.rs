436fa0763c12f10910b998efa3c8fb4ad3d10d36
docherak <docherak@protonmail.ch>
2025-02-03 17:54:34 +0100

    feat: add handling of path commands for type builtin

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,28 @@ use std::io::{self, Write};
 
 use std::process;
 use std::iter::Iterator;
+use std::env;
+use std::path::Path;
+
+fn get_path_dirs() -> Option<Vec<String>> {
+    env::var("PATH").ok().map(|path| {
+        path.split(':')
+            .map(|s| s.trim().to_string())
+            .collect()
+    })
+}
+
+fn command_in_path(command: &str, path: Option<Vec<String>>) -> Option<String> {
+    path?.into_iter()
+        .find_map(|dir| {
+            let full_path = format!("{}/{}", dir, command);
+            if Path::new(&full_path).exists() {
+                Some(full_path)
+            } else {
+                None
+            }
+        })
+}
 
 fn main() {
     let stdin = io::stdin();
@@ -35,10 +57,16 @@ fn main() {
                 println!("{}", message);
             },
             Some("type") => {
-                if let Some(command) = parts.next() {
+                while let Some(command) = parts.next() {
                     match command {
                         "exit" | "echo" | "type" => println!("{} is a shell builtin", command),
-                        _ => println!("{}: not found", command)
+                        _ => {
+                            if let Some(path) = command_in_path(command, get_path_dirs()) {
+                                println!("{} is {}", command, path);
+                            } else {
+                                println!("{}: not found", command);
+                            }
+                        }
                     }
                 }
             }
