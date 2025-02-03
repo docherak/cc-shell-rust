24cb53fc383d3548fedd1b63df50fe70711372f4
docherak <docherak@protonmail.ch>
2025-02-03 13:06:09 +0100

    refactor: loop over input

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@
 use std::io::{self, Write};
 
 fn main() {
+    let stdin = io::stdin();
+    let mut input = String::new();
 
     loop {
+        input.clear();
         print!("$ ");
         io::stdout().flush().unwrap();
-        let stdin = io::stdin();
-        let mut input = String::new();
         stdin.read_line(&mut input).unwrap();
         println!("{}: command not found", input.trim());
     }
